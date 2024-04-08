6d3450aa1f19bb2c33b9a3295360e0d409d184f9
dinaberikkyzy1 <200103045@stu.sdu.edu.kz>
2024-04-08 09:15:46 +0500

    export tp pdf

--- diplomaproject/projectapp/urls.py
+++ diplomaproject/projectapp/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from . import views
 urlpatterns = [
     path("", include("diplomaproject.urls")),
     path("admin/", admin.site.urls),
+    path('onboarding/step1/', views.onboarding_step1, name='onboarding_step1'),
+    path('onboarding/step2/', views.onboarding_step2, name='onboarding_step2'),
+    path('onboarding/step3/', views.onboarding_step3, name='onboarding_step3'),
+    path('onboarding/complete/', views.onboarding_complete, name='onboarding_complete'),
+    path('exportpdf/', views.export_to_pdf, name='export_to_pdf'),
 ]
--- diplomaproject/projectapp/views.py
+++ diplomaproject/projectapp/views.py
@@ -1,14 +1,22 @@
-import csv
+
 import os
 import re
+
 import PyPDF2
 from django.http import JsonResponse
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Table
 
-# Create your views here.
+
+
+
 
 @csrf_exempt
 def convert_pdf_to_json(request):
@@ -20,55 +28,59 @@ def convert_pdf_to_json(request):
             # Save the PDF file temporarily
             temp_path = default_storage.save(os.path.join(settings.MEDIA_ROOT, 'temp.pdf'), ContentFile(pdf_file.read()))
 
-            # Extract tables from PDF
+            # Return the JSON response
             tables = extract_tables_from_pdf(temp_path)
+            print(tables)
+            json_response = convert_tables_to_json(tables)
 
-            # Convert tables to JSON
-            json_data = convert_tables_to_json(tables)
-
-            # Export JSON data to CSV
-            csv_file_path = os.path.join(settings.MEDIA_ROOT, 'output.csv')
-            csv_exported_path = export_json_to_csv(json_data, csv_file_path)
-
-            # Return CSV file path
-            return JsonResponse({'data': json_data})
+            return JsonResponse(json_response, safe=False)
 
     return JsonResponse({'error': 'Invalid request'}, status=400, safe=False)
-
 
 def extract_tables_from_pdf(temp_path):
     pdf_file = open(temp_path, 'rb')
     pdf_reader = PyPDF2.PdfReader(pdf_file)
     num_pages = len(pdf_reader.pages)
+    print(num_pages)
     tables = []
-
     for page_number in range(num_pages):
         page_text = pdf_reader.pages[page_number].extract_text()
         page_tables = extract_tables_from_page_text(page_text)
-        if page_number == 0:
-            continue
         tables.extend(page_tables)
-
     pdf_file.close()
-
-    tables.sort(key=lambda x: x[0].split()[0])
-
     return tables
 
-
-
 def extract_tables_from_page_text(page_text):
+    # Regular expression to identify simple table-like structures
+    # This is just a basic example and might need to be adjusted based on the actual PDF structure
+    #table_regex = r'\d+\s+[A-Za-z]+\s+\d+\s+'  # Example: 1   Item A    100
+    table_regex = r'\d+\s+[A-Za-z]+\s+Replenishment\s+\d+\s+'
+
     tables = []
-
+    # lines = page_text.split('\n')
+    # current_table = []
+    # for line in lines:
+    #     if re.match(table_regex, line):
+    #         current_table.append(line.strip())
+    #     elif current_table:
+    #         tables.append(current_table)
+    #         current_table = []
+    # if current_table:
+    #     tables.append(current_table)
+    ##################
     lines = page_text.split('\n')
-
     current_table = []
-
     for line in lines:
-        current_table.append(line.strip())
+        # if re.match(table_regex, line):
+        if 'Replenishment' in line:
 
+            current_table.append(line.strip())
+        # elif current_table:
+        #     tables.append(current_table)
+    # if current_table:
     tables.append(current_table)
 
+    ###########
     return tables
 
 def convert_tables_to_json(tables):
@@ -76,39 +88,96 @@ def convert_tables_to_json(tables):
     del (tables[0][0])
     for table in tables:
         for row in table:
-            if '₸' not in row: continue
-            if "JSC" in row: continue
-
+                # Extracting relevant information from the table row
             date = row[:8]
-            comma_index = row.find(",")
+            amount = "".join([i for i in row if i.isnumeric()][6:-2])
 
-            amount = "".join([i for i in row][9:comma_index])
+            if "Income" in row:
+                category = "Income"
+            elif "Outcome" in row:
+                category = "Outcome"
+            elif "Replenishment" in row:
+                category = "Replenishment"
 
-            transaction_type = ""
 
             if "+" in row:
-                transaction_type = "Income"
+               type = "Income"
             elif "-" in row:
-                transaction_type = "Outcome"
+               type = "Outcome"
 
             json_entry = {
                 "date": date,
-                "amount": int(''.join(re.findall('\d+', amount))),
-                "type": transaction_type,
-                "category": re.sub(' +', ' ', row.split('₸', 1)[1].strip()).split(' ', 1)[0]
+                "amount": amount,
+                "type": type,
+                "category": category
             }
-
             json_data.append(json_entry)
     return json_data
 
+    return JsonResponse({'error': 'Invalid request'}, status=400,safe=False)
 
-def export_json_to_csv(json_data, csv_file_path):
-    fieldnames = ['date', 'amount', 'type', 'category']
 
-    with open(csv_file_path, mode='w', newline='',encoding='utf-8') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(json_data)
 
-    return csv_file_path
 
+
+def onboarding_step1(request):
+    if request.method == 'POST':
+        form = IncomeForm(request.POST)
+        if form.is_valid():
+            income = form.save(commit=False)
+            income.user = request.user
+            income.save()
+            return redirect('onboarding_step2')
+    else:
+        form = IncomeForm()
+    return render(request, 'onboarding_step1.html', {'form': form})
+
+def onboarding_step2(request):
+    if request.method == 'POST':
+        form = SpendingForm(request.POST)
+        if form.is_valid():
+            spending = form.save(commit=False)
+            spending.user = request.user
+            spending.save()
+            return redirect('onboarding_step3')
+    else:
+        form = SpendingForm()
+    return render(request, 'onboarding_step2.html', {'form': form})
+
+def onboarding_step3(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user
+            review.save()
+            return redirect('onboarding_complete')
+    else:
+        form = ReviewForm()
+    return render(request, 'onboarding_step3.html', {'form': form})
+
+def onboarding_complete(request):
+    return render(request, 'onboarding_complete.html')
+
+
+def export_to_pdf(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="data.pdf"'
+
+    doc = SimpleDocTemplate(response, pagesize=letter)
+    elements = []
+
+    data = [[key, str(value)] for key, value in json_data.items()]
+    table = Table(data, colWidths=[200, 200])
+    table.setStyle([('BACKGROUND', (0, 0), (-1, 0), colors.gray),
+                    ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+                    ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                    ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+                    ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+                    ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+                    ('GRID', (0, 0), (-1, -1), 1, colors.black)])
+
+    elements.append(table)
+
+    doc.build(elements)
+    return response
